a8c4a3174e81099e0f4d5109f6724d91759edb25
debarpanRoy2006 <debarpanroy984@gmail.com>
2025-07-30 19:41:50 +0530

    Improving Ui

--- agent_ai_project/wsgi.py
+++ agent_ai_project/wsgi.py
@@ -11,6 +11,7 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'agent_ai_project.settings')
+settings_module= 'agent_ai_project.deployment_settings' if 'RENDER_EXTERNAL_HOSTNAME' in os.environ else 'agent_ai_project.settings'
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', settings_module)
 
 application = get_wsgi_application()
